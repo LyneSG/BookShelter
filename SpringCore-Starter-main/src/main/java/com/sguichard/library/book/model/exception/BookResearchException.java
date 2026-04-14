e841a226a2eb43c46bae58453466cac0d2cd43ad
Sylvain G. <sylvaing.2006@gmail.com>
2026-04-14 10:06:31 +0200

    reinit

--- SpringCore-Starter-main/src/main/java/com/sguichard/library/book/model/exception/BookResearchException.java
+++
@@ -1,7 +0,0 @@
-package com.sguichard.library.book.model.exception;
-
-public class BookResearchException extends RuntimeException {
-    public BookResearchException(String message) {
-        super(message);
-    }
-}
