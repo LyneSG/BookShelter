efb5fa3d843ed93bbbd2305a102b7896cbbf76d4
Sylvain G. <sylvaing.2006@gmail.com>
2026-04-14 13:06:30 +0200

    Request COR

--- SpringCore-Starter-main/src/main/java/com/sguichard/library/book/controllers/BookRestController.java
+++ SpringCore-Starter-main/src/main/java/com/sguichard/library/book/controllers/BookRestController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/book")
+@CrossOrigin(origins= "http://localhost:4200")
+@RequestMapping("/books")
 public class BookRestController {
 
     private final BookService bookService;
